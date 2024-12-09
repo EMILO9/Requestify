d4a285388596f7ff62c36ae8997d5cbf0ce8d602
EMILO9 <emilberolsen@gmail.com>
2024-12-09 14:53:38 +0100

    d

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,11 @@ import RequestifyError from "./requestifyError";
 export default function routes(r: Route[]) {
   return (routes: Route[]) => {
     try {
-      r.push(...routes);
+      const routesDef = routes.map((route) => ({
+        ...route,
+        method: route.method ?? "GET",
+      }));
+      r.push(...routesDef);
     } catch (error) {
       throw new RequestifyError((error as Error).message);
     }
